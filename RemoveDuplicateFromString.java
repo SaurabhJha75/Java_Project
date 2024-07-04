2ec6a95e24a71342a2ce742ba07bad736702c581
Saurabh Kumar Jha <59827666+SaurabhJha75@users.noreply.github.com>
2024-07-04 20:16:24 +0530

    Add files via upload
    We will see in this program that how we remove duplicate character from a string in Java.

---
+++ RemoveDuplicateFromString.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class RemoveDuplicateFromString {
+
+	public static String removeDuplicate(String str) {
+
+		char charArray[] = str.toCharArray();
+
+		String result = "";
+
+		Set<Character> set = new HashSet<Character>();
+
+		for (char ch : charArray) {
+			if (set.add(ch)) {
+				result = result + ch;
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a string");
+		String str = sc.nextLine();
+		
+		System.out.println("String without duplicate character: " + removeDuplicate(str));
+	}
+
+}
